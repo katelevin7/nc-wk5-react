1f9b0c4dfeaede0a4bdbb22b5d0c88c2274413a5
KateLevin <kate.levin7@gmail.com>
2023-02-04 12:28:07 -0500

    initialize repo

---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,73 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import { CAMPSITES } from '../../app/shared/CAMPSITES'
+import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImageURL } from '../../utils/mapImageURL';
+
+export const fetchCampsites = createAsyncThunk(
+    'campsites/fetchCampsites',
+    async () => {
+        const response = await fetch(baseUrl + 'campsites');
+        if (!response.ok) {
+            return Promise.reject('Unable to fetch, status: ' + response.status);
+        }
+        const data = await response.json();
+        return data;
+    }
+);
+
+const initialState = {
+    campsitesArray: [],
+    isLoading: true,
+    errMsg: ''
+};
+
+export const selectAllCampsites = (state) => {
+    return state.campsites.campsitesArray;
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [fetchCampsites.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [fetchCampsites.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = '';
+            state.campsitesArray = mapImageURL(action.payload);
+        },
+        [fetchCampsites.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = action.error ? action.error.message : 'Fetch failed';
+        }
+    }
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
+
+export const selectCampsiteById = (id) => (state) => {
+    console.log('campsiteSlice', state.campsites.campsitesArray)
+    return state.campsites.campsitesArray.find(
+        (campsite) => campsite.id === parseInt(id)
+     );
+};
+
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+// };
+
+// export const selectFeaturedCampsite = (state) => {
+//     return state.campsites.campsitesArray.find((campsite) => campsite.featured);
+// };
+
+export const selectFeaturedCampsite = (state) => {
+    return {
+        featuredItem: state.campsites.campsitesArray.find(
+            (campsite) => campsite.featured
+        ),
+        isLoading: state.campsites.isLoading,
+        errMsg: state.campsites.errMsg
+    };
+};
